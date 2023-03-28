e0cb2c3a1f2f35ab982d15a1b73c3674abbcf83d
Fr33Ze06 <118484751+Fr33Ze06@users.noreply.github.com>
2023-03-28 23:32:39 +0200

    Update snake.js

--- static/snake.js
+++ static/snake.js
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     if (!end){
-        var jeu = setInterval(update, 75);
+        var jeu = setInterval(update, 100);
     }
     
     //Function
